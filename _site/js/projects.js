cabb29bccb7c63874dde8ffc115a5ef0539f2814
wgallop <Will.Gallop@benefitfocus.com>
2016-05-14 12:06:38 -0400

    altered gitignore

---
+++ _site/js/projects.js
@@ -0,0 +1,19 @@
+( function (document, window, index)
+	{
+
+	// 'use strict';
+
+	// 	var current = 0,
+	// 	slides = document.querySelectorAll(".featured-project__slide");
+
+	// 	setInterval(function() {
+	// 		for (var i = 0; i < slides.length; i++) {
+	// 		slides[i].style.opacity = 0;
+	// 	}
+		
+	// 		current = (current != slides.length - 1) ? current + 1 : 0;
+	// 		slides[current].style.opacity = 1;
+
+	// 	}, 3000);
+
+}( document, window, 0 ));
